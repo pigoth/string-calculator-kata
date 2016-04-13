55d46c762f2cb986302a6fc73a41dc17cdc3de28
unknown <TFerrari@FM03.factorymind.intranet>
2016-04-13 17:46:29 +0200

    Step 1

---
+++ Code/MyCalculator/MyCalculator.Test/KataCalculatorTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyCalculator.Test
+{
+    [TestClass]
+    public class KataCalculatorTest
+    {
+        [TestMethod]
+        public void StringEmptyTest()
+        {
+            var result = KataStringCalculator.Add(string.Empty);
+            const int expectedResult = 0;
+            Assert.AreEqual(expectedResult, result);
+        } 
+        
+        [TestMethod]
+        public void AddOneNumberString()
+        {
+            var result = KataStringCalculator.Add("1");
+            const int expectedResult = 1;
+            Assert.AreEqual(expectedResult, result);
+        }
+        
+        [TestMethod]
+        public void AddTwoNumbersSeparatedByComma()
+        {
+            var result = KataStringCalculator.Add("1,2");
+            const int expectedResult = 0;
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ Code/MyCalculator/MyCalculator/KataStringCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCalculator
+{
+    public static class KataStringCalculator
+    {
+        public static int Add(string numbers, char separator = ',')
+        {
+            var rtnValue = 0;
+
+            if (!string.IsNullOrEmpty(numbers))
+            {
+                var splittedNumbers = numbers.Split(separator);
+                foreach (var stringNumber in splittedNumbers)
+                {
+                    int numberToAdd;
+                    if (int.TryParse(stringNumber, out numberToAdd))
+                        rtnValue += rtnValue;
+                }
+            }
+
+            return rtnValue;
+        }
+    }
+}
